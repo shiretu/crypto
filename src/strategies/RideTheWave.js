bc1abeac03f42189fe1c66d6f23e179648293e98
Eugen-Andrei Gavriloaie <andrei@gavriloaie.com>
2025-10-12 16:17:55 +0300

    -- renamed stuff

--- src/strategies/RideTheWave.js
+++ src/strategies/RideTheWave.js
@@ -2,8 +2,8 @@ class RideTheWave {
     static SETUP = {
         pattern: {
             firstIncreaseCandlesCount: 3,
-            allowDipLimit: 0.7,
-            dipCountLimit: 3,
+            decreasePercentLimit: 0.7,
+            decreaseCountLimit: 3,
             greenTransactionsCount: 200
         },
         order: {
@@ -20,18 +20,18 @@ class RideTheWave {
         this.events.on('candleUpdate', (evt) => this.#onCandleUpdate(evt))
         this.events.on('candleClose', (evt) => this.#onCandleClose(evt))
         this.events.on('orderClosed', (evt) => { this.enabled = true })
-        this.candlesFirstIncrease = []
-        this.candlesDip = []
-        this.candlesSecondIncrease = []
+        this.firstIncrease = []
+        this.decrease = []
+        this.secondIncrease = []
         this.enabled = true
     }
 
     #reset (reason) {
         if (!reason) { throw new Error('No reason given') }
         // console.log(reason)
-        this.candlesFirstIncrease = []
-        this.candlesDip = []
-        this.candlesSecondIncrease = []
+        this.firstIncrease = []
+        this.decrease = []
+        this.secondIncrease = []
     }
 
     #onCandleClose (evt) {
@@ -47,69 +47,69 @@ class RideTheWave {
             this.#reset('Green candle almost yellow: too few trades')
             return
         }
-        if (this.candlesFirstIncrease.length === 0) {
-            this.candlesFirstIncrease.push(evt.candle.clone())
+        if (this.firstIncrease.length === 0) {
+            this.firstIncrease.push(evt.candle.clone())
             return
         }
-        if (this.candlesDip.length === 0) {
-            this.candlesFirstIncrease.push(evt.candle.clone())
+        if (this.decrease.length === 0) {
+            this.firstIncrease.push(evt.candle.clone())
         } else {
-            this.candlesSecondIncrease.push(evt.candle.clone())
+            this.secondIncrease.push(evt.candle.clone())
         }
     }
 
     #onCandleCloseZero (evt) {
-        if (this.candlesFirstIncrease.length === 0) { return }
-        if (this.candlesDip.length === 0) {
-            this.candlesFirstIncrease.push(evt.candle.clone())
+        if (this.firstIncrease.length === 0) { return }
+        if (this.decrease.length === 0) {
+            this.firstIncrease.push(evt.candle.clone())
             return
         }
-        if (this.candlesSecondIncrease.length === 0) {
-            if (this.candlesDip.length >= RideTheWave.SETUP.pattern.dipCountLimit) {
+        if (this.secondIncrease.length === 0) {
+            if (this.decrease.length >= RideTheWave.SETUP.pattern.decreaseCountLimit) {
                 this.#reset()
                 return
             }
-            this.candlesDip.push(evt.candle.clone())
+            this.decrease.push(evt.candle.clone())
             return
         }
-        this.candlesSecondIncrease.push(evt.candle.clone())
+        this.secondIncrease.push(evt.candle.clone())
     }
 
     #onCandleCloseRed (evt) {
         // if we get the red too soon, we bail out
-        if (this.candlesFirstIncrease.length < RideTheWave.SETUP.pattern.firstIncreaseCandlesCount) {
+        if (this.firstIncrease.length < RideTheWave.SETUP.pattern.firstIncreaseCandlesCount) {
             this.#reset('First increase is not having enough green candles')
             return
         }
 
-        // see if we dip too much
-        const totalHeight = this.candlesFirstIncrease.at(-1).close.price - this.candlesFirstIncrease[0].open.price
-        const dipLimit = this.candlesFirstIncrease[0].open.price + totalHeight * RideTheWave.SETUP.pattern.allowDipLimit
-        if (evt.candle.close.price <= dipLimit) {
-            this.#reset('The dip went too far')
+        // see if we decreased too much
+        const totalHeight = this.firstIncrease.at(-1).close.price - this.firstIncrease[0].open.price
+        const decreaseLimit = this.firstIncrease[0].open.price + totalHeight * RideTheWave.SETUP.pattern.decreasePercentLimit
+        if (evt.candle.close.price <= decreaseLimit) {
+            this.#reset('The decrease went too far')
             return
         }
 
-        // is this the first dip? if so, we store it and we are done
-        if (this.candlesDip.length === 0) {
-            this.candlesDip.push(evt.candle.clone())
+        // is this the first decrease? if so, we store it and we are done
+        if (this.decrease.length === 0) {
+            this.decrease.push(evt.candle.clone())
             return
         }
 
         // did we already started to add to second increase?
-        if (this.candlesSecondIncrease.length !== 0) {
+        if (this.secondIncrease.length !== 0) {
             this.#reset()
             return
         }
 
-        // do we have more then 3 dips?
-        if (this.candlesDip.length >= RideTheWave.SETUP.pattern.dipCountLimit) {
-            this.#reset('We have too many dips')
+        // do we have too many decreases?
+        if (this.decrease.length >= RideTheWave.SETUP.pattern.decreaseCountLimit) {
+            this.#reset('We have too many decreases')
             return
         }
 
         // add it
-        this.candlesDip.push(evt.candle.clone())
+        this.decrease.push(evt.candle.clone())
     }
 
     #onCandleOpen (evt) {
@@ -127,15 +127,15 @@ class RideTheWave {
     }
 
     #onCandleUpdateGreen (evt) {
-        // did we get the dip?
-        if (this.candlesDip.length === 0) { return }
+        // did we get the decrease?
+        if (this.decrease.length === 0) { return }
 
         // are we below or equal last high?
-        const buyAt = this.candlesDip.map(candle => candle.high.price).sort((a, b) => b - a)[0]
+        const buyAt = this.decrease.map(candle => candle.high.price).sort((a, b) => b - a)[0]
         if (buyAt >= evt.tick.price) { return }
 
         // establish the protection sell
-        const sellAtLow = this.candlesDip.map(candle => candle.low.price).sort().at(0)
+        const sellAtLow = this.decrease.map(candle => candle.low.price).sort().at(0)
         const sellAtHigh = buyAt * (1 + RideTheWave.SETUP.order.transactionFeesPercent) * (1 + RideTheWave.SETUP.order.gainsPercent) / (1 - RideTheWave.SETUP.order.transactionFeesPercent)
         // console.log(((sellAtHigh - buyAt) / buyAt * 100).toFixed(2))
 
@@ -151,10 +151,10 @@ class RideTheWave {
             },
             metadata: {
                 tick: evt.tick,
-                startTsHr: this.candlesFirstIncrease[0].open.tsHr,
+                startTsHr: this.firstIncrease[0].open.tsHr,
                 buyTsHr: evt.tick.tsHr,
-                firstIncrease: this.candlesFirstIncrease,
-                dip: this.candlesDip,
+                firstIncrease: this.firstIncrease,
+                decrease: this.decrease,
                 secondIncrease: this.secondIncrease
             }
         })
@@ -165,7 +165,7 @@ class RideTheWave {
     #onCandleUpdateZero (evt) {}
 
     #onCandleUpdateRed (evt) {
-        if (this.candlesSecondIncrease.length !== 0) {
+        if (this.secondIncrease.length !== 0) {
             this.#reset('We have received a red candle on the second increase')
         }
     }
